043ac5f4801a3f6a72b2a19a40cc8b12911f5a0f
lilian-ljungkvist <57765690+lilian-ljungkvist@users.noreply.github.com>
2020-08-30 15:22:21 -0700

    add action_page.php script

--- action_page.php
+++ action_page.php
@@ -1,1 +1,16 @@
+<?php
 
+if(isset($_POST['submit']))
+{
+$name = $_POST['fname'];
+$emailFrom = $_POST['email'];
+$inquiry = $_POST['inquiry'];
+$message = $_POST['message'];
+
+$mailTo = "lilian.ljungkvist@gmail.com";
+$headers = "From: ".$emailFrom;
+$txt = "Recieved email from: ".$name.".\n\n".$message;
+
+mail($mailTo, $subject, $txt, $headers);
+header("Location: contact.html?mailsend")
+}
